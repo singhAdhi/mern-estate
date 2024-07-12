d6834337690549e3801ceb5001dfdfba65de520e
Adhiraj Singh <adhirajsingh7303@gmail.com>
2024-07-12 19:21:32 +0530

    header UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./component/Header";
 
 const App = () => {
-  return <div className="text-red-300">app</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
